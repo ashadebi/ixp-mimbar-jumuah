01a42c1d5b80e902dc75c0565b2bc7a905ce43dd
Agoes <ashadebi@github.com>
2026-09-17 00:11:44 +0000

    chore: add db schema and default admin seed

---
+++ dashboard/seed_admin.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (id, name, email, password_hash, role) VALUES (1, 'Super Admin', 'admin@mimbar.local', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'super-admin');
